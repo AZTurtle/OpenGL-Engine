acbdb19810f8533935c85b11648d50ce07684387
AZTurtle <71404928+AZTurtle@users.noreply.github.com>
2023-01-30 02:11:32 +0000

    Setup

--- include/resources.h
+++ include/resources.h
@@ -7,16 +7,10 @@
 
 namespace shaders{
 	extern ShaderBase<uniforms<glm::mat4*, float*,Texture*>,staticUniforms<>> diffuse2D;
-	extern ShaderBase<uniforms<glm::mat4*>,staticUniforms<>> diffuse;
-	extern ShaderBase<uniforms<glm::mat4*>,staticUniforms<>> letterShader;
-	extern ShaderBase<uniforms<glm::mat4*, Texture*>,staticUniforms<glm::mat4*>> textured;
-	extern ShaderBase<uniforms<float*>,staticUniforms<>> fadeShader;
 };
 
 namespace models{
-	extern Model_def_N letters[26];
-	extern Model_def_NT l1;
-	extern Model_def_NT table;
+
 };
 
 #endif
